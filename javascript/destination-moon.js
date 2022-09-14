87cae9cdff6249f5d0a0c73bfa7725a81554e051
Gavin Lang <gavincollislang@gmail.com>
2022-09-14 11:18:04 -0700

    working on making a template page for every destination page

--- javascript/destination-moon.js
+++ javascript/destination-moon.js
@@ -1,19 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "../data.json");
 
-  <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
-  <link rel="stylesheet" href="style/style.css">
-  <link rel="stylesheet" href="style/mobile-style.css">
-  <link rel="stylesheet" href="style/mobile-destination.css">
-  <script src="./javascript/mobile.js" defer></script>
-  <script src="./javascript/destination-moon.js" defer></script>
-  <title>Frontend Mentor | Space tourism website</title>
-</head>
-<body>
-  <!-- <nav>  
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+        renderHTML(data);
+    }
+}
+
+xhr.send();
+
+
+function renderHTML(data) {
+    let body = document.querySelector("body");
+    body.innerHTML = 
+    `<nav>  
     <a href="index.html"><svg id="logo" xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg></a>
     <svg id="hamburger" xmlns="http://www.w3.org/2000/svg" width="24" height="21"><g fill="#D0D6F9" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z"/></g></svg>
     <aside id="nav-modal">
@@ -43,17 +45,15 @@
       <h5>01</h5>
       <h5>Pick Your Destination</h5>
     </header>
-    <img id="planet" src="./assets/destination/image-moon.png" alt="moon">
+    <img id="planet" src="${data.destinations[0].images.png}" alt="${data.destinations[0].name}">
     <ol class="second-nav-items">
       <a href="./destination-moon.html"><li class="current"><h5>Moon</h5></li></a>
       <a href="./destination-mars.html"><li><h5>Mars</h5></li></a>
       <a href="./destination-europa.html"><li><h5>Europa</h5></li></a>
       <a href="./destination-titan.html"><li><h5>Titan</h5></li></a>
     </ol>
-    <h1>Moon</h1>
-    <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
-      regain perspective and come back refreshed. While you’re there, take in some history 
-      by visiting the Luna 2 and Apollo 11 landing sites.</p>
+    <h1>${data.destinations[0].name}</h1>
+    <p>${data.destinations[0].description}</p>
       <hr>
     <div class="stat">
       <H2 class="subheader2">Avg. distance</H2>
@@ -64,7 +64,5 @@
       <H1 class="subheader1">3 days</H1>
     </div>
 
-  </main> -->
-
-</body>
-</html>
+  </main>`
+}
